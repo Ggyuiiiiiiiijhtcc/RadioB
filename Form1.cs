8774b6253155de0c3ff9e89582044d415de805f3
Джанклод Вандам <93411756+Ggyuiiiiiiiijhtcc@users.noreply.github.com>
2023-06-20 23:37:20 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,18 @@
+namespace RadioButtons
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            var temp = sender as RadioButton;
+            this.BackColor = temp.ForeColor;
+        }
+
+        
+    }
+}
